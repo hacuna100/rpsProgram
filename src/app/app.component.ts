1bc7d7a27028203f6fdc9e5badbc4c26acd2a966
Hermilo Acuna <hacuna100@gmail.com>
2023-12-03 21:15:51 -0700

    90% solution to the RPS program. Final adjustments needed (formatting and commenting)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -10,5 +10,111 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  pResult = "❔";
+  cResult = "❔";
+  resultMsg = "Best 2 out of 3 wins";
   title = 'rpsProgram';
+  drawMsg = "Its a Draw!";
+  winMsg = "You won the round!";
+  loseMsg = "You lost the round!";
+  gameWinMsg = "You won the game!";
+  gameLoseMsg = "You lost the game!";
+  pWins = 0;
+  cWins = 0;
+  pMax = 2;
+  cMax = 2;
+
+
+  getComputerChoice() {
+    let choice;
+
+    choice = Math.floor(Math.random() * 3)
+
+    switch (choice) {
+      case 0:
+        this.cResult = "✊";
+        break;
+      case 1:
+        this.cResult = "✋";
+        break;
+      case 2:
+        this.cResult = "✌️";
+        break;
+    }
+
+    this.cResult;
+  }
+
+  rockChoice() {
+    this.pResult = "✊";
+    this.getComputerChoice();
+
+    switch (this.cResult) {
+      case "✊":
+        this.resultMsg = this.drawMsg;
+        break;
+      case "✋":
+        this.resultMsg = this.loseMsg;
+        this.cWins++;
+        break;
+      case "✌️":
+        this.resultMsg = this.winMsg;
+        this.pWins++
+        break;
+    }
+  }
+
+  paperChoice() {
+    this.pResult = "✋";
+    this.getComputerChoice();
+
+    switch (this.cResult) {
+      case "✊":
+        this.resultMsg = this.winMsg;
+        this.pWins++
+        break;
+      case "✋":
+        this.resultMsg = this.drawMsg;
+        break;
+      case "✌️":
+        this.resultMsg = this.loseMsg;
+        this.cWins++;
+        break;
+    }
+  }
+
+  scissorChoice() {
+    this.pResult = "✌️";
+    this.getComputerChoice();
+
+    switch (this.cResult) {
+      case "✊":
+        this.resultMsg = this.loseMsg;
+        this.cWins++;
+        break;
+      case "✋":
+        this.resultMsg = this.winMsg;
+        this.pWins++;
+        break;
+      case "✌️":
+        this.resultMsg = this.drawMsg;
+        break;
+    }
+  }
+
+  winResult() {
+    return this.pWins == this.pMax;
+  }
+
+  loseResult() {
+    return this.cWins == this.cMax;
+  }
+
+  playAgain() {
+    this.pResult = "❔";
+    this.cResult = "❔";
+    this.pWins = 0;
+    this.cWins = 0;
+  }
 }
+
